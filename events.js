7f65e61dd00c0255870d99521adeab0b8aabb09c
Reaceee <davecod9@gmail.com>
2023-07-16 18:43:15 +0100

    Final Debug

--- events.js
+++ events.js
@@ -1,35 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let triggeredLink = false
+// document.addEventListener("DOMContentLoaded", () => {
+//     let triggeredLink = false
 
-    document.body.addEventListener('mousemove', () => {
-        if (triggeredLink === false) {
-            if (document.querySelectorAll('.compLink').length > 2) {
-                triggeredLink = true
-                // console.log('Mapped Successfully')
+//     document.body.addEventListener('mousemove', () => {
+//         if (triggeredLink === false) {
+//             if (document.querySelectorAll('.compLink').length > 2) {
+//                 triggeredLink = true
+//                 // console.log('Mapped Successfully')
 
-                document.querySelectorAll('.compLink').forEach(link => {
-                    link.addEventListener('click', () => {
-                        document.querySelector('.underline').style.left = link.id + 'px'
-                    })
-                }
-                )
+//             }
 
-            }
+//         }
+//     })
+// })
 
-        }
-    })
-})
-
-setInterval(() => {
-    // console.log(window.scrollY)
-    if (window.scrollY < 300) {
-        document.querySelector('.scrollBtn').style.bottom = '-2.5rem'
-    }
-    else {
-        document.querySelector('.scrollBtn').style.display = 'flex'
-        document.querySelector('.scrollBtn').style.bottom = '2.5rem'
+// setInterval(() => {
+//     // console.log(window.scrollY)
+//     if (window.scrollY < 300) {
+//         document.querySelector('.scrollBtn').style.bottom = '-2.5rem'
+//     }
+//     else {
+//         document.querySelector('.scrollBtn').style.display = 'flex'
+//         document.querySelector('.scrollBtn').style.bottom = '2.5rem'
 
 
 
-    }
-}, 300)
+//     }
+// }, 300)
--- src/components/miniComp.jsx
+++ src/components/miniComp.jsx
@@ -1,9 +1,10 @@
-
 const miniComp = (props) => {
-    return(
-        <div id={props.id} className="compLink flex flex-col gap-4 py-6 w-[80px] items-center cursor-pointer text-[.9em] hover:text-gray-600 text-center">
-            <img src={props.icon} alt="icon" width={25} />
-            <p>{props.title}</p>
+
+
+    return (
+        <div onClick={(e) => { document.querySelector('.underline').style.left = e.target.id + 'px' }} id={props.id} className="compLink flex flex-col gap-4 py-6 w-[80px] items-center cursor-pointer text-[.9em] hover:text-gray-600 text-center">
+            <img onClick={(e) => { document.querySelector('.underline').style.left = e.target.id + 'px' }} id={props.id} src={props.icon} alt="icon" width={25} />
+            <p onClick={(e) => { document.querySelector('.underline').style.left = e.target.id + 'px' }} id={props.id}>{props.title}</p>
         </div>
     )
 }
